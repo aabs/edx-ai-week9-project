c2eef1172ce3af53181213e3973e28e2bee1f01c
Andrew Matthews <matthews.andrew@gmail.com>
2017-03-18 07:06:26 +1100

    init import

---
+++ driver_3.py
@@ -0,0 +1,16 @@
+def ac3(csp)->bool:
+    pass
+
+def revise(csp, Xi, Xj) -> bool:
+    pass
+
+def main():
+    # parse problem string
+    # build representation
+    # solve problem
+    # compare with solution
+    # display some output??
+    pass
+
+if __name__ == "__main__":
+    main()
